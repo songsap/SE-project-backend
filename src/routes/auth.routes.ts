6194afd20dd9d78521c57f60e4d1bef4c7a93cfb
2ugarZ3ro <6631303021@student.chula.ac.th>
2025-09-12 21:12:50 +0700

    add update restaurant

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { registerRestaurant, login, me, logout } from '../controllers/auth.controller';
+import { registerRestaurant, login, me, logout, getMyRestaurant, updateMyRestaurant } from '../controllers/auth.controller';
 import auth from '../middlewares/auth';
+import { requireRole } from '../middlewares/role';          
+import { useRestaurantScope } from '../middlewares/scope';  
 
 const r = Router();
 r.post('/register-restaurant', registerRestaurant);
 r.post('/login', login);
 r.get('/me', auth, me);
 r.post('/logout', auth, logout);
+r.get('/restaurant/me', auth, requireRole('restaurant'), useRestaurantScope, getMyRestaurant);
+r.put('/restaurant/me', auth, requireRole('restaurant'), useRestaurantScope, updateMyRestaurant);
+
 export default r;
